6e57f04f495221aae48bce6da0cc235e5fc14605
Max <Max@Max-PC.cable.rcn.com>
2015-11-17 22:49:15 -0500

    incrmented progress

--- Assignmnet5/src/Heap.java
+++ Assignmnet5/src/Heap.java
@@ -1,26 +1,72 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
-public class Heap implements PriQueueInterface {
+public class Heap<E extends Comparable<E>> implements PriQueueInterface<E> {
+
+	/**
+	 * I looked online to understand how to make java accept a generic array:
+	 */
+	private E[] elements;
+
+	public Heap(Class<E> c, int size) {
+		@SuppressWarnings("unchecked")
+		final E[] elements = (E[]) Array.newInstance(c, size);
+		this.elements = elements;
+	}
 
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
+		//if there is an element inside, return false
+		for(@SuppressWarnings("unused") E elem : elements){
+			return false;
+		}
+		return true;
 	}
 
 	@Override
 	public boolean isFull() {
-		// TODO Auto-generated method stub
-		return false;
+		int i = 0;
+		for(E element : elements){
+			i++;
+		}
+		return i == elements.length;
 	}
 
 	@Override
-	public void enqueue(Comparable element) {
-		// TODO Auto-generated method stub
+	public void enqueue(E element) {
+	
+		
+			recEnqeue(0, element);
+		
+
+	}
+	
+	private boolean recEnqeue(int index, E element){
+
+		if(elements[index] == null){
+			elements[index] = element;
+			return true;
+		}else{
+		return recEnqeue(rightChild(index), element) 
+				|| recEnqeue(leftChild(index), element);
+		
+		}
 		
 	}
+	
+	private int leftChild(int index){
+		return index * 2;
+	}
+	
+	private int rightChild(int index){
+		return (2 * index) + 1; 
+	}
+	
+	
+	
 
 	@Override
-	public Comparable dequeue() {
+	public E dequeue() {
 		// TODO Auto-generated method stub
 		return null;
 	}
