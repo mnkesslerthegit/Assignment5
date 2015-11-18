cbe2d4a8b4b05ed4ff553ebfd35944b3f6879f2c
Max <Max@Max-PC.cable.rcn.com>
2015-11-17 22:08:09 -0500

    added interface

---
+++ Assignmnet5/src/PriQueueInterface.java
@@ -0,0 +1,27 @@
+public interface PriQueueInterface<T extends Comparable<T>>
+
+{
+
+	boolean isEmpty();
+
+	// Returns true if this priority queue is empty, false otherwise.
+
+	boolean isFull();
+
+	// Returns true if this priority queue is full, false otherwise.
+
+	void enqueue(T element);
+
+	// Throws PriQOverflowException if this priority queue is full;
+
+	// otherwise, adds element to this priority queue.
+
+	T dequeue();
+
+	// Throws PriQUnderflowException if this priority queue is empty;
+
+	// otherwise, removes element with highest priority from this
+
+	// priority queue and returns it.
+
+}
