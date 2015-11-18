3f84ad287a7e9218b4559d90bbbe2816a78a0cfc
Max <Max@Max-PC.cable.rcn.com>
2015-11-17 22:09:20 -0500

    implemented interface

---
+++ Assignmnet5/src/Heap.java
@@ -0,0 +1,28 @@
+
+public class Heap implements PriQueueInterface {
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isFull() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void enqueue(Comparable element) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Comparable dequeue() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
