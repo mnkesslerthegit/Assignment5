f947378dd56f69db3b6eb0d1c256181ae7271a74
mnkesslerthegit <Max@10.28.51.149>
2015-11-20 17:20:23 -0500

    organizating

--- Assignmnet5/src/dataStructures/Heap.java
+++ Assignmnet5/src/dataStructures/Heap.java
@@ -1,3 +1,4 @@
+package dataStructures;
 import java.lang.reflect.Array;
 import java.util.Arrays;
 
--- Assignmnet5/src/dataStructures/PriQueueInterface.java
+++ Assignmnet5/src/dataStructures/PriQueueInterface.java
@@ -1,3 +1,4 @@
+package dataStructures;
 public interface PriQueueInterface<T extends Comparable<T>>
 
 {
