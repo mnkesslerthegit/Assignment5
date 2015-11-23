13ed24543986784431f2119fd5f1d353e0664436
mnkesslerthegit <Max@10.28.51.149>
2015-11-23 16:45:40 -0500

    Heap is now tested

--- Assignmnet5/src/dataStructures/Heap.java
+++ Assignmnet5/src/dataStructures/Heap.java
@@ -1,87 +1,202 @@
 package dataStructures;
 
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Heap<E extends Comparable<E>> implements PriQueueInterface<E> {
 
-	/**
-	 * I looked online to understand how to make java accept a generic array:
-	 */
-	private E[] elements;
+	private ArrayList<E> myArray = new ArrayList<>();
 
-	@SuppressWarnings("unchecked")
-	public Heap(int size) {
-		elements = (E[]) new Comparable[size];
-
+	public String toString() {
+		return myArray.toString();
 	}
 
-	public E[] toArray() {
-		E[] result = Arrays.<E>copyOf(elements, elements.length);
-		return result;
+	public Object[] toArray() {
+		return myArray.toArray();
+	}
+
+	public boolean contains(E element) {
+		return myArray.contains(element);
+	}
+
+	/**
+	 * I use an array list to simulate an array, because working with generic
+	 * arrays in java is too hard.
+	 * 
+	 * @param initSize
+	 */
+	public Heap(int initSize) {
+		for (int i = 0; i < initSize; i++) {
+			myArray.add(null);
+		}
 	}
 
 	@Override
 	public boolean isEmpty() {
-		// if there is an element inside, return false
-		for (@SuppressWarnings("unused")
-		E elem : elements) {
-			return false;
+		for(int i = 0; i < myArray.size(); i++){
+			if(myArray.get(i)!= null){
+				return false;
+			}
 		}
 		return true;
-	}
-
-	@Override
-	public boolean isFull() {
-		int i = 0;
-		for (E element : elements) {
-			i++;
-		}
-		return i == elements.length;
+		
 	}
 
 	@Override
 	public void enqueue(E element) {
+		System.out.println("Enqueing");
+		if (!isFull()) {
+			if (!recEnqeue(1, element)) {
+				throw new IllegalArgumentException("Failed to enque when heap wasn't full");
+			}
+		}
+		System.out.println(myArray);
+		// printArray();
+	}
 
-		recEnqeue(0, element);
-
+	private void printArray() {
+		System.out.println();
+		for (int i = 0; i < myArray.size(); i++) {
+			System.out.println("I is: " + i + " value is: " + myArray.get(i));
+		}
+		System.out.println();
 	}
 
 	/**
-	 * Add an element to the Heap. 
+	 * Add an element to the Heap.
 	 * 
 	 * @param index
 	 * @param element
 	 * @return
 	 */
 	private boolean recEnqeue(int index, E element) {
-		if (index >= elements.length) {
+		if (index >= myArray.size()) {
 			return false;
-		
-		}else if (elements[index] == null) {
-			elements[index] = element;
-			return true;
-		
 		} else {
+			// System.out.println("Index is: " + index + " parent is: " +
+			// parent(index));
+			// System.out.println("Index value is: " + myArray.get(index)+ "
+			// parent value is: " + myArray.get(parent(index)));
 
-			return recEnqeue(rightChild(index), element) || recEnqeue(leftChild(index), element);
+			// check for space, then balance.
+			if (myArray.get(index) == null) {
+				myArray.set(index, element);
+				System.out.println("Storing " + element + " in " + index);
+				balanceUp(index);
+
+				return true;
+			}
+			// we don't try to access children of null parents
+			return myArray.get(index) != null && recEnqeue(rightChild(index), element)
+					|| recEnqeue(leftChild(index), element);
 
 		}
 
 	}
 
+	private void balanceUp(int index) {
+		// check if element is violating heap order
+		if (myArray.get(index).compareTo(myArray.get(parent(index))) < 0) {
+			// if it is, swap it with its parent
+			swap(index, parent(index));
+			// sift up
+			balanceUp(parent(index));
+
+		}
+	}
+
+	private void swap(int i1, int i2) {
+		E temp = myArray.get(i2);
+		myArray.set(i2, myArray.get(i1));
+		myArray.set(i1, temp);
+	}
+
+	/**
+	 * 
+	 * @param index
+	 */
+	private void balanceDown(int index) {
+		System.out.println(myArray);
+		if(index == 0){
+			return;
+		}
+		/**
+		 * Check to see if the index has children. If it does, pick the one with the smaller value, and swap them. 
+		 */
+		if (hasLeft(index)) {
+			if(!hasRight(index)){
+				
+			swap(index, leftChild(index));
+			balanceDown(leftChild(index));
+			}else{
+				if(myArray.get(leftChild(index)).compareTo(myArray.get(rightChild(index))) < 1){
+					swap(index, leftChild(index));
+					balanceDown(leftChild(index));
+				}else{
+					swap(index, rightChild(index));
+					balanceDown(rightChild(index));
+				}
+				
+			}
+		} else if (hasRight(index)) {
+			swap(index, rightChild(index));
+			balanceDown(rightChild(index));
+		}
+		
+
+
+	}
+
+	// if we are at index 1, consider the parent to be index 1
+	private int parent(int index) {
+		if (index <= 1) {
+			return 1;
+		} else {
+			return index / 2;
+		}
+
+	}
+
+	private boolean hasLeft(int index) {
+		return leftChild(index) < myArray.size() && myArray.get(leftChild(index)) != null;
+	}
+
+	private boolean hasRight(int index) {
+		return rightChild(index) < myArray.size() && myArray.get(rightChild(index)) != null;
+	}
+
 	private int leftChild(int index) {
+
 		return index * 2;
 	}
 
 	private int rightChild(int index) {
+
 		return (2 * index) + 1;
 	}
 
 	@Override
 	public E dequeue() {
-		// TODO Auto-generated method stub
-		return null;
+		System.out.println("Deque begins:\n");
+		if (myArray.isEmpty()) {
+			throw new IndexOutOfBoundsException("Can't deque from empty heap");
+		}
+		E temp = myArray.get(1);
+		// myArray.set(0 Comparable<>);
+		myArray.set(1, null);
+		balanceDown(1);
+		return temp;
+	}
+
+	@Override
+	public boolean isFull() {
+		for (int i = 1; i < myArray.size(); i++) {
+			if (myArray.get(i) == null) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 }
--- Assignmnet5/src/tests/HeapTest.java
+++ Assignmnet5/src/tests/HeapTest.java
@@ -2,80 +2,186 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import java.util.Random;
+
+import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import dataStructures.Heap;
 
 public class HeapTest {
-	
 
-	private int leftChild(int index){
+	private int leftChild(int index) {
+		// index++;
 		return index * 2;
 	}
-	
-	private int rightChild(int index){
-		return (2 * index) + 1; 
-	}
-	
 
-	private <T extends Comparable<T>> boolean testHeightRelation(T upper, T lower ){
-		
-		return upper.compareTo(lower) == 1;
-		
+	private int rightChild(int index) {
+		// index++;
+		return (2 * index) + 1;
 	}
+
 	Heap<String> myHeap;
 	String[] myArray;
-	private int heapSize = 11;
-	
+	private int heapSize = 10;
+
 	@Before
-	public void setUp(){
-		myHeap = new Heap<>(heapSize );
+	public void setUp() {
+		myHeap = new Heap<>(heapSize);
+		myArray = new String[heapSize];
+
 		/**
-		 * Add strings to the heap
+		 * Add some strings to the heap
 		 */
-		for(int i = 0; i < heapSize; i++){
-			myHeap.enqueue("" + (char) 65 + i );
+		for (int i = 0; i < heapSize / 2; i++) {
+
+			myHeap.enqueue("" + (char) (65 + i));
 		}
-		System.out.print(myHeap.toArray());
-		myArray =  myHeap.toArray();
-		
+
 	}
-	
-	@Test
-	public void testHeapOrder() {
-		for(int i = 0; i < myArray.length; i++){
+
+	/**
+	 * I test heapOrder after every test.
+	 */
+	@After
+	public void tearDown() {
+
+		// setRegularArray();
+		testHeapOrder();
+
+		myHeap = null;
+	}
+
+	private void setRegularArray() {
+		myArray = new String[heapSize];
+
+		Object[] temp = myHeap.toArray();
+		// System.out.println("temp is: " + temp);
+		if (!myHeap.isEmpty()) {
+			for (int i = 0; i < heapSize; i++) {
+				if (temp[i] != null) {
+					// System.out.println("got here: " + temp[i]);
+					myArray[i] = (String) temp[i];
+				} else {
+					myArray[i] = null;
+				}
+			}
+		}
+	}
+
+	private <T extends Comparable<T>> boolean testHeightRelation(T upper, T lower) {
+
+		System.out.println("upper: " + upper + " lower: " + lower);
+		System.out.println(upper.compareTo(lower));
+
+		return upper.compareTo(lower) > 0;
+
+	}
+
+	private void testHeapOrder() {
+		setRegularArray();
+		System.out.println("\n");
+		System.out.println("Heap order test");
+		for (int i = 1; i < myArray.length; i++) {
 			int left = leftChild(i);
 			int right = rightChild(i);
-			if(testHeightRelation(myArray[i], myArray[left])
-					&& testHeightRelation(myArray[i], myArray[right])){
-				
-			}else{
-				fail("Heap order violated at index" + i + "element " + myArray[i]);
+			System.out.println("HeapOrder reached " + i + ", element: " + myArray[i]);
+
+			// check for a null parent
+			if (left < myArray.length && myArray[left] != null) {
+				if (myArray[i] == null) {
+					fail("null parent at index" + i + " where left child is " + myArray[left]);
+				}
 			}
-			
-			
+
+			if (right < myArray.length && myArray[right] != null) {
+				if (myArray[i] == null) {
+					fail("null parent at index" + i + " where right child is " + myArray[right]);
+				}
+			}
+
+			if (myArray[i] == null) {
+				continue;
+			}
+			// System.out.println("left is: " + left);
+			// System.out.print("Array length is: " + myArray.length);
+
+			// check for a smaller parent
+			if (left < myArray.length) {
+				if (myArray[left] != null && testHeightRelation(myArray[i], myArray[left])) {
+
+					fail("Heap order violated at index" + i + " element " + myArray[i] + "\n" + "at left child, index"
+							+ left + " element " + myArray[left]);
+				}
+			}
+			if (right < myArray.length) {
+				if (myArray[right] != null && testHeightRelation(myArray[i], myArray[right])) {
+
+					fail("Heap order violated at index" + i + " element " + myArray[i] + "\n" + "at right child, index"
+							+ right + "element" + myArray[right]);
+				}
+			}
+
 		}
 	}
-	
+
 	@Test
 	public void testIsEmpty() {
-		fail("Not yet implemented");
+		for (int i = 0; i < heapSize * 2; i++) {
+
+			myHeap.dequeue();
+		}
+		Assert.assertEquals(true, myHeap.isEmpty());
 	}
 
 	@Test
 	public void testIsFull() {
-		fail("Not yet implemented");
+		for (int i = 0; i < heapSize * 2; i++) {
+
+			myHeap.enqueue("" + (char) (65 + randy.nextInt(30)));
+		}
+		Assert.assertEquals(true, myHeap.isFull());
 	}
+
+	Random randy = new Random();
 
 	@Test
 	public void testEnqueue() {
-		fail("Not yet implemented");
+
+		myHeap.enqueue("Need");
+		for (int i = 0; i < heapSize * 2; i++) {
+
+			myHeap.enqueue("" + (char) (65 + randy.nextInt(30)));
+		}
+		Assert.assertEquals(true, (myHeap.contains("Need"))); // testIsFull();
+
 	}
 
 	@Test
 	public void testDequeue() {
-		fail("Not yet implemented");
+		String testString = "Need";
+		myHeap.enqueue(testString);
+		for (int i = 0; i < heapSize * 3; i++) {
+			try {
+				myHeap.dequeue();
+			} catch (IndexOutOfBoundsException e) {
+
+			}
+			testHeapOrder();
+		}
+
+		Assert.assertEquals(false, (myHeap.contains(testString)));
+		Assert.assertEquals(true, (myHeap.isEmpty()));
+
+		myHeap.enqueue(testString);
+		Assert.assertEquals(true, (myHeap.contains(testString)));
+		// System.out.print(myHeap);
+		// String result = myHeap.dequeue();
+		Assert.assertTrue(myHeap.dequeue().equals(testString));
+		Assert.assertEquals(false, (myHeap.contains(testString)));
+
 	}
 
 }
