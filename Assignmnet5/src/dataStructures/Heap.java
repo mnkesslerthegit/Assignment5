d6ae753992036b258528373ebf133874bf35dbc9
mnkesslerthegit <Max@10.28.51.149>
2015-11-22 19:44:48 -0500

    asdf

--- Assignmnet5/src/dataStructures/Heap.java
+++ Assignmnet5/src/dataStructures/Heap.java
@@ -1,4 +1,5 @@
 package dataStructures;
+
 import java.lang.reflect.Array;
 import java.util.Arrays;
 
@@ -9,16 +10,22 @@ public class Heap<E extends Comparable<E>> implements PriQueueInterface<E> {
 	 */
 	private E[] elements;
 
-	public Heap(Class<E> c, int size) {
-		@SuppressWarnings("unchecked")
-		final E[] elements = (E[]) Array.newInstance(c, size);
-		this.elements = elements;
+	@SuppressWarnings("unchecked")
+	public Heap(int size) {
+		elements = (E[]) new Comparable[size];
+
+	}
+
+	public E[] toArray() {
+		E[] result = Arrays.<E>copyOf(elements, elements.length);
+		return result;
 	}
 
 	@Override
 	public boolean isEmpty() {
-		//if there is an element inside, return false
-		for(@SuppressWarnings("unused") E elem : elements){
+		// if there is an element inside, return false
+		for (@SuppressWarnings("unused")
+		E elem : elements) {
 			return false;
 		}
 		return true;
@@ -27,7 +34,7 @@ public class Heap<E extends Comparable<E>> implements PriQueueInterface<E> {
 	@Override
 	public boolean isFull() {
 		int i = 0;
-		for(E element : elements){
+		for (E element : elements) {
 			i++;
 		}
 		return i == elements.length;
@@ -35,36 +42,41 @@ public class Heap<E extends Comparable<E>> implements PriQueueInterface<E> {
 
 	@Override
 	public void enqueue(E element) {
-	
-		
-			recEnqeue(0, element);
-		
+
+		recEnqeue(0, element);
 
 	}
-	
-	private boolean recEnqeue(int index, E element){
 
-		if(elements[index] == null){
+	/**
+	 * Add an element to the Heap. 
+	 * 
+	 * @param index
+	 * @param element
+	 * @return
+	 */
+	private boolean recEnqeue(int index, E element) {
+		if (index >= elements.length) {
+			return false;
+		
+		}else if (elements[index] == null) {
 			elements[index] = element;
 			return true;
-		}else{
-		return recEnqeue(rightChild(index), element) 
-				|| recEnqeue(leftChild(index), element);
 		
+		} else {
+
+			return recEnqeue(rightChild(index), element) || recEnqeue(leftChild(index), element);
+
 		}
-		
+
 	}
-	
-	private int leftChild(int index){
+
+	private int leftChild(int index) {
 		return index * 2;
 	}
-	
-	private int rightChild(int index){
-		return (2 * index) + 1; 
+
+	private int rightChild(int index) {
+		return (2 * index) + 1;
 	}
-	
-	
-	
 
 	@Override
 	public E dequeue() {
--- Assignmnet5/src/tests/HeapTest.java
+++ Assignmnet5/src/tests/HeapTest.java
@@ -2,12 +2,79 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import dataStructures.Heap;
+
 public class HeapTest {
+	
+
+	private int leftChild(int index){
+		return index * 2;
+	}
+	
+	private int rightChild(int index){
+		return (2 * index) + 1; 
+	}
+	
+
+	private <T extends Comparable<T>> boolean testHeightRelation(T upper, T lower ){
+		
+		return upper.compareTo(lower) == 1;
+		
+	}
+	Heap<String> myHeap;
+	String[] myArray;
+	private int heapSize = 11;
+	
+	@Before
+	public void setUp(){
+		myHeap = new Heap<>(heapSize );
+		/**
+		 * Add strings to the heap
+		 */
+		for(int i = 0; i < heapSize; i++){
+			myHeap.enqueue("" + (char) 65 + i );
+		}
+		System.out.print(myHeap.toArray());
+		myArray =  myHeap.toArray();
+		
+	}
+	
+	@Test
+	public void testHeapOrder() {
+		for(int i = 0; i < myArray.length; i++){
+			int left = leftChild(i);
+			int right = rightChild(i);
+			if(testHeightRelation(myArray[i], myArray[left])
+					&& testHeightRelation(myArray[i], myArray[right])){
+				
+			}else{
+				fail("Heap order violated at index" + i + "element " + myArray[i]);
+			}
+			
+			
+		}
+	}
+	
+	@Test
+	public void testIsEmpty() {
+		fail("Not yet implemented");
+	}
 
 	@Test
-	public void test() {
+	public void testIsFull() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEnqueue() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDequeue() {
 		fail("Not yet implemented");
 	}
 
